6508d1e9ca875f36edc7a3df2d77739e1a4aeb92
May Ng <mngmay@gmail.com>
2019-07-15 13:15:33 -0700

    Added quarter button. Quarter increments when clicked

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+  const [quarter, setQuarter] = useState(1);
 
   return (
     <div className="container">
@@ -25,9 +26,16 @@ function App() {
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow quarter={quarter} />
       </section>
       <section className="buttons">
+        <button
+          onClick={() => setQuarter(quarter + 1)}
+          className="quarterButton"
+        >
+          Quarter
+        </button>
+
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button
